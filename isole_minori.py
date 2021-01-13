649ff479b66c17ab5f98e3ec6a5757ae98e8149d
Antonio Vitale <65810505+vitalelele@users.noreply.github.com>
2021-01-13 12:48:25 +0100

    Update isole_minori.py

--- isole_minori.py
+++ isole_minori.py
@@ -5,14 +5,14 @@ cap_isole = []
 
 with open("CAP_italiani_lista.txt", "r") as cap_italia:
     cap_italiani = [item.replace(",\n", "") for item in cap_italia]
-print(cap_italiani)
+# print(cap_italiani) 
 
 with open("isoleminori.txt", "r") as isole:
     cap_isole = [item.replace(",\n", "") for item in isole]
-print(cap_isole)
+# print(cap_isole)
 
 cap_senza_isole = [item.replace(",", "") for item in cap_italiani if item not in cap_isole]
-print(cap_senza_isole)
+# print(cap_senza_isole)
 
 # se volessi creare un file txt con i CAP
 with open("cap_senza_isole.txt", "w") as file:
